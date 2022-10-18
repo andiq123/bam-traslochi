92ec945d01ef220b81c89a49f61a3236f03765a0
andiq123 <andy.u789@gmail.com>
2022-10-18 18:55:13 +0300

    cargo png fix

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
-import cargo from "../../public/images/cargo.png";
+import cargo from "../public/images/cargo.png";
 import Image from "next/image";
 import Link from "next/link";
 
