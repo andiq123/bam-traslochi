33506ddcc6c29815222e666c12cf564d793b22b6
andiq123 <andy.u789@gmail.com>
2022-12-18 13:35:55 +0200

    applied google script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { ToastContainer } from "react-toastify";
 import { useEffect } from "react";
 import { insertVisitor } from "../lib/firebaseApiWrapper";
+import Script from "next/script";
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -19,6 +20,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <ToastContainer />
       <Component {...pageProps} />
+      {/* <!-- Google tag (gtag.js) - Google Analytics --> */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-239327884-1"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-239327884-1');`}
+      </Script>
     </Layout>
   );
 }
